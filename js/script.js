184b2645fb70f0dbd82e892d93ae9cc13b3fc38d
David Tees <34275197+Sawus@users.noreply.github.com>
2023-09-10 12:18:58 -0400

    Rewrite script.js
    - Changes to JS file
    - Updated Games List
    - Pictures working in Game Pages

--- js/script.js
+++ js/script.js
@@ -1,60 +1,52 @@
 // Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Init Variables
-var hamburger;
-var homeButton;
-var sidebar;
-var sidebarContainer = [];
-var gamesContainer = [];
-var sidebarGames;
-var sidebarApps;
-var sidebarModels;
-var sidebarAbout;
-var content;
-var currentHash;
-var leftArrow;
-var rightArrow;
+var x = window.matchMedia("(min-width: 1025px)");
+var running = false;
 
 var gameCatalog = {
+    gooseInc: {
+        imgSrc: ["images/gooseInc/GooseInc_0.png", "images/gooseInc/GooseInc_1.png", "images/gooseInc/GooseInc_2.png", "images/gooseInc/GooseInc_3.png", "images/gooseInc/GooseInc_4.png", "images/gooseInc/GooseInc_5.png", "images/gooseInc/GooseInc_6.png"],
+        imgIndex: 0,
+        content: "html/games/gooseInc.html",
+    },
+    modifiedRoll: {
+        imgSrc: ["images/tearDevil/TearDevil_0.png", "images/tearDevil/TearDevil_1.png", "images/tearDevil/TearDevil_2.png"],
+        imgIndex: 0,
+        content: "html/games/modifiedRoll.html",
+    },
     tearDevil: {
-        imgSrc: ["images/TearDevil_0.png", "images/TearDevil_1.png", "images/TearDevil_2.png"],
+        imgSrc: ["images/tearDevil/TearDevil_0.png", "images/tearDevil/TearDevil_1.png", "images/tearDevil/TearDevil_2.png"],
         imgIndex: 0,
         content: "html/games/tearDevil.html",
-        //hash: element.id
     },
     jumpGame: {
-        imgSrc: ["images/TearDevil_0.png", "images/TearDevil_1.png", "images/TearDevil_2.png"],
+        imgSrc: ["images/tearDevil/TearDevil_0.png", "images/tearDevil/TearDevil_1.png", "images/tearDevil/TearDevil_2.png"],
         imgIndex: 0,
         content: "html/games/jumpGame.html",
     },
     isoStrategy: {
-        imgSrc: ["images/TearDevil_0.png", "images/TearDevil_1.png", "images/TearDevil_2.png"],
+        imgSrc: ["images/tearDevil/TearDevil_0.png", "images/tearDevil/TearDevil_1.png", "images/tearDevil/TearDevil_2.png"],
         imgIndex: 0,
         content: "html/games/isoStrategy.html",
     },
     mathGame: {
-        imgSrc: ["images/TearDevil_0.png", "images/TearDevil_1.png", "images/TearDevil_2.png"],
+        imgSrc: ["images/tearDevil/TearDevil_0.png", "images/tearDevil/TearDevil_1.png", "images/tearDevil/TearDevil_2.png"],
         imgIndex: 0,
         content: "html/games/mathGame.html",
     }
 }
 
-var x = window.matchMedia("(min-width: 1025px)");
-    
-var running = true;
-
 // JQuery Load Shared HTML
 $.get("html/shared.html", function(data){
     $("#shared").replaceWith(data);
     onLoad();
-    //onLoadGames();
-    GetHash();
+    getHash();
 });
 
 window.addEventListener('hashchange',() =>{
     //console.log('The URL has has changed');
-    GetHash();
+    getHash();
 });
 
 window.onresize = function (event) {
@@ -64,28 +56,34 @@ window.onresize = function (event) {
 
 function updateHamburger() {
     //console.log("Clicked");
-    if (running) {
-        running = false;
+    let hamburger = getHamburger();
+    if (!running) {
+        running = true;
         if (hamburger.className != "hamburger hamburger--elastic is-active")
             hamburger.className = "hamburger hamburger--elastic is-active";
         else
             hamburger.className = "hamburger hamburger--elastic";
         sidebarSlide();
         setTimeout(function () {
-            running = true;
+            running = false;
         }, 500)
     }
 }
 
 function sidebarSlide() {
+    let sidebar = getSidebar();
+    let content = getContent();
+    let hamburger = getHamburger();
+
     var openPosition = -sidebar.shadow;
     var closePosition = -sidebar.width;
+    
     if (hamburger.className == "hamburger hamburger--elastic is-active") {
         if (x.matches) {
             content.style.marginLeft = "184px";
             sidebar.style.width = "180px";
         } else {
-            sidebar.style.transition = "0.5s";
+            //sidebar.style.transition = "0.5s";
             sidebar.style.width = "100%";
         }
         sidebar.style.marginLeft = "184px";
@@ -96,59 +94,87 @@ function sidebarSlide() {
             content.style.marginLeft = "0px";
             sidebar.style.width = "180px";
         } else {
-            sidebar.style.transition = "0.5s";
+            //sidebar.style.transition = "0.5s";
             content.style.marginLeft = "0px";
             sidebar.style.marginLeft = "-100%";
         }
     }
 }
 
-function onNavigationClick(event) {
-    
-    switchContent(event.currentTarget.content)
-    // Extra Functionality for Games 
-    if(event.currentTarget.id == sidebarContainer.find(object => object.hash === "Games").id) {
-        setTimeout(function () {
-            onLoadGames();
-        }, 500) 
-    }  
-
-    location.hash = event.currentTarget.hash;
-}
-function switchContent(content)
+function onLoad()
 {
-    // Replace Date in Content
-    $.get(content, function(data){
-        $("#content").replaceWith(data);
-    });
-    // Automatically Close Menu
-    // if(event.currentTarget.id.includes("sidebar"))
-    // {
-        
-    // }
-    if (hamburger.className == "hamburger hamburger--elastic is-active")
-        updateHamburger();
+    // Hamburger
+    let hamburger = getHamburger();
+    hamburger.addEventListener("click", updateHamburger);
+
+    // Sidebar
+    let sidebar = getSidebar();
+    sidebar.width = 180;
+    sidebar.shadow = 4;
+    sidebar.style.height = window.innerHeight - 60 + 'px';
+    sidebar.style.width = sidebar.width + 'px';
+    sidebar.style.left = '-' + (sidebar.width + sidebar.shadow) + 'px';
+    
+    // Content
+    let content = getContent();
+    content.style.height = window.innerHeight - 60 + 'px';
+    content.style.left = 0 + 'px';
+    content.style.marginLeft = 0 + 'px';
+
+    // Home
+    let home = getButton("Home");
+    home.addEventListener("click", onNavigationClick);
+    home.content = "html/home.html";
+    home.hash = "Home";
+    
+
+
+    //Games
+    let games = getButton("Games");
+    games.addEventListener("click", onNavigationClick);
+    games.content = "html/games.html";
+    games.hash = "Games";
+
+    // Apps
+    let apps = getButton("Apps");
+    apps.addEventListener("click", onNavigationClick);
+    apps.content = "html/apps.html";
+    apps.hash = "Apps";
+
+    // 3D Models
+    let models = getButton("Models");
+    models.addEventListener("click", onNavigationClick);
+    models.content = "html/models.html";
+    models.hash = "Models";
+
+    // About Me
+    let about = getButton("About");
+    about.addEventListener("click", onNavigationClick);
+    about.content = "html/aboutMe.html";
+    about.hash = "About";
+
 }
 
 function onLoadGames() {
-    var gridContainer = document.querySelector(".gamesGridContainer");
-    var gameContainer = gridContainer.querySelectorAll(".gameMenu");
-    // Iterate Through Menus
+    let gridContainer = document.querySelector(".gamesGridContainer");
+    console.log(gridContainer);
+    let gameContainer = gridContainer.querySelectorAll(".gameMenu");
+    //Iterate Through Menus
     gameContainer.forEach(element => {
         // Title
-        var title = element.querySelector(".gameTitle");
+        let title = element.querySelector(".gameTitle");
         title.addEventListener("click", onNavigationClick);
         title.content = "html/games/" + element.id + ".html";
         title.hash = element.id;
-        gamesContainer.push(title);
+        //gamesContainer.push(title);
         
         // Pictures
-        var pictureContainer = element.querySelector(".pictureGridContainer")
-        var leftArrow = pictureContainer.querySelector(".leftArrow");
+        let pictureContainer = element.querySelector(".pictureGridContainer")
+        let leftArrow = pictureContainer.querySelector(".leftArrow");
         leftArrow.addEventListener("click", switchImage);
         leftArrow.modifier = -1;
         leftArrow.owner = element;
-        var rightArrow = pictureContainer.querySelector(".rightArrow");
+        let rightArrow = pictureContainer.querySelector(".rightArrow");
         rightArrow.addEventListener("click", switchImage);
         rightArrow.modifier = 1;
         rightArrow.owner = element;
@@ -160,9 +186,164 @@ function onLoadGames() {
     }
 }
 
+function onLoadGame(name)
+{
+    let pictureContainer = document.querySelector(".pictureGridContainer")
+    let leftArrow = pictureContainer.querySelector(".leftArrow");
+    leftArrow.addEventListener("click", switchImage);
+    leftArrow.modifier = -1;
+    console.log(gameCatalog[name]);
+    leftArrow.owner = pictureContainer;
+    let rightArrow = pictureContainer.querySelector(".rightArrow");
+    rightArrow.addEventListener("click", switchImage);
+    rightArrow.modifier = 1;
+    rightArrow.owner = pictureContainer;
+
+        // Reset Values
+    for (var game in gameCatalog) {
+        gameCatalog[game].imgIndex = 0;
+    }
+}
+
+function getSidebar()
+{
+    return document.getElementById("sidebar");
+}
+
+function getContent()
+{
+    return document.getElementById("content");
+}
+
+function getHamburger()
+{
+    return document.getElementById("hamburger");
+}
+
+function getHome()
+{
+    return document.getElementById("homeButton");
+}
+function getButton(value)
+{
+    let button;
+    switch(value)
+    {
+        case "Home":
+            button = document.getElementById("homeButton");
+            break;
+        case "Games":
+            button = document.getElementById("sidebarGames");
+            break;
+        case "Apps":
+            button = document.getElementById("sidebarApps");
+            break;
+        case "Models":
+            button = document.getElementById("sidebarModels");
+            break;
+        case "About":
+            button = document.getElementById("sidebarAbout");
+            break;
+        default:
+            button = document.getElementById("homeButton");
+    }
+    //console.log(button);
+    return button;
+}
+
+function getHash()
+{
+    
+    if(Object.keys(gameCatalog).includes(location.hash.slice(1)))
+    {
+        for (let propName in gameCatalog)
+        {
+            if(propName === location.hash.slice(1))
+            {
+                switchContent(gameCatalog[propName].content);
+                
+                setTimeout(function () {
+                    onLoadGame(propName);
+                }, 500)
+                
+                return;
+            }
+                
+        }
+        console.log("Its a Game!");
+        //console.log()
+    }
+    else
+    {
+        let content = getButton(location.hash.slice(1))
+        switchContent(content.content);
+    }
+    
+    // let object = sidebarContainer.find(object => location.hash.includes(object.hash));
+    // if(object)
+    // {
+    //     console.log(object.hash);
+    //     object.click();
+    //     return;
+    // }
+    // console.log(gameCatalog[location.hash.slice(1)])
+    // object = gameCatalog[location.hash.slice(1)]
+    // //object = gameCatalog.find(object => location.hash.includes(object.id));
+    // if(object) 
+    // {
+    //     switchContent(object.content);
+    //     //object.click();
+    // }
+    
+    // else
+    // {
+    //     switchContent(home.content);
+    // }
+}
+
+function onNavigationClick(event) {
+    
+    // switchContent(event.currentTarget.content)
+    // // Extra Functionality for Games 
+    // if(event.currentTarget.hash == "Games") {
+    //     setTimeout(function () {
+    //         onLoadGames();
+    //     }, 500) 
+    // }  
+
+    location.hash = event.currentTarget.hash;
+    console.log(event.currentTarget.hash);
+}
+
+function switchContent(content)
+{
+    // Replace Date in Content
+    $.get(content, function(data){
+        $("#content").replaceWith(data);
+    });
+
+    if(content === "html/games.html")
+    {
+        setTimeout(function () {
+            onLoadGames();
+        }, 500)
+    }
+    // Automatically Close Menu
+    // if(event.currentTarget.id.includes("sidebar"))
+    // {
+        
+    // }
+    let hamberger = getHamburger();
+    if (hamburger.className == "hamburger hamburger--elastic is-active")
+        updateHamburger();
+}
+
 function switchImage(event) {
     // Gets Game That Event Was Called For
     var game = gameCatalog[event.currentTarget.owner.id];
+
+    if(!game)
+        console.log("nada")
     // Change Index
     game.imgIndex += event.currentTarget.modifier;
     // If Higher Or Lower Then Amount of Pictures, Reset
@@ -173,94 +354,4 @@ function switchImage(event) {
     // Update Current Picture
     event.currentTarget.owner.querySelector(".gamePictures").src = game.imgSrc[game.imgIndex];
     console.log(event.currentTarget.owner.id + ": " + game.imgIndex);
-}
-
-function onLoad() {
-    hamburger = document.getElementById("hamburger");
-    hamburger.addEventListener("click", updateHamburger);
-    // Home
-    var home = document.getElementById("homeButton");
-    home.addEventListener("click", onNavigationClick);
-    home.content = "html/home.html";
-    home.hash = "Home";
-    sidebarContainer.push(home);
-
-
-    // Games
-    var games = document.getElementById("sidebarGames");
-    games.addEventListener("click", onNavigationClick);
-    games.content = "html/games.html";
-    games.hash = "Games";
-    sidebarContainer.push(games);
-    // Apps
-    var apps = document.getElementById("sidebarApps");
-    apps.addEventListener("click", onNavigationClick);
-    apps.content = "html/apps.html";
-    apps.hash = "Apps";
-    sidebarContainer.push(apps);
-    // 3D Models
-    var models = document.getElementById("sidebarModels");
-    models.addEventListener("click", onNavigationClick);
-    models.content = "html/models.html";
-    models.hash = "Models";
-    sidebarContainer.push(models);
-    // About Me
-    var about = document.getElementById("sidebarAbout");
-    about.addEventListener("click", onNavigationClick);
-    about.content = "html/aboutMe.html";
-    about.hash = "About";
-    sidebarContainer.push(about);
-    
-    sidebar = document.getElementById("sidebar");
-    sidebar.width = 180;
-    sidebar.shadow = 4;
-    
-    content = document.getElementById("content");
-
-    //Jquery Replace Content With Home Page
-    $.get("html/home.html", function(data){    
-        $("#content").replaceWith(data);
-    });
-
-    sidebar.style.height = window.innerHeight - 60 + 'px';
-    sidebar.style.width = sidebar.width + 'px';
-    sidebar.style.left = '-' + (sidebar.width + sidebar.shadow) + 'px';
-    content.style.height = window.innerHeight - 60 + 'px';
-    content.style.left = 0 + 'px';
-    content.style.marginLeft = 0 + 'px';
-}
-
-function GetHash()
-{
-    // console.log(location.hash);
-    // sidebarContainer.forEach(element => {
-    //     if(location.hash.includes(element.hash))
-    //     {
-    //         console.log("Found It!");
-    //     }
-    //     console.log(element.hash);
-    // });
-    let object = sidebarContainer.find(object => location.hash.includes(object.hash));
-    if(object)
-    {
-        console.log(object.hash);
-        object.click();
-        return;
-    }
-    console.log(gameCatalog[location.hash.slice(1)])
-    object = gameCatalog[location.hash.slice(1)]
-    //object = gameCatalog.find(object => location.hash.includes(object.id));
-    if(object) 
-    {
-        switchContent(object.content)
-        //object.click();
-    }
-
-    // for (const property in sidebarContainer) {
-    //     console.log(property);
-    //     if(location.hash.includes(property.hash))
-    //     {
-    //         console.log(`${property}: ${sidebarContainer[property]}`);
-    //     }
-    //   }
 }
